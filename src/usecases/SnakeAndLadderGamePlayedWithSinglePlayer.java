39d8c930334b9e5b8620f688c627965cc49bdc4d
sambit420 <sambit2024@gmail.com>
2022-10-08 13:15:27 +0530

    Added : UC-1 Snake and laddergame played with single player at the start position 0

---
+++ src/usecases/SnakeAndLadderGamePlayedWithSinglePlayer.java
@@ -0,0 +1,14 @@
+package useCases;
+
+public class SnakeAndLadderGamePlayedWithSinglePlayer {
+    SnakeAndLadderGamePlayedWithSinglePlayer()
+    {
+
+        System.out.println("Welcome to Snake and Ladder Simulation \n");
+    }
+    public static void main(String[] args) {
+        SnakeAndLadderGamePlayedWithSinglePlayer a = new SnakeAndLadderGamePlayedWithSinglePlayer();
+        int startingPosition = 0;
+        System.out.println("Starting Position of the Single Player is : " + startingPosition);
+    }
+}
